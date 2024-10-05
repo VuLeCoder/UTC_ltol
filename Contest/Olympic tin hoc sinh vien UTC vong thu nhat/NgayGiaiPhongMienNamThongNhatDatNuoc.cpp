4e611f7a766998b5ea9b857b5de83b349b43f755
VuzLe <tle63917@gmail.com>
2024-10-05 21:31:40 +0700

    Create

---
+++ Contest/Olympic tin hoc sinh vien UTC vong thu nhat/NgayGiaiPhongMienNamThongNhatDatNuoc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int x; cin >> x;
+    int ans = x - 1975;
+    cout << "Ky niem " << ans << " nam ngay Giai phong Mien Nam, thong nhat dat nuoc";
+
+    return 0;
+}
