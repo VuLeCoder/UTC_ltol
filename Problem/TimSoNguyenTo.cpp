90f4f25d7b51557152ca6475eaeb2ad056be2493
VuzLe <tle63917@gmail.com>
2024-10-05 21:16:53 +0700

    Create (0, 1 is not prime)

--- Problem/TimSoNguyenTo.cpp
+++ Problem/TimSoNguyenTo.cpp
@@ -7,9 +7,10 @@ bool prime[MAX];
 void Sang_so_nguyen_to()
 {
     memset(prime, true, sizeof(prime));
+    prime[0] = prime[1] = false;
     for (int i = 2; i * i < MAX; ++i)
         if (prime[i])
-            for (int j = i * i; j < MAX; j += i)
+            for (int j = i*i; j < MAX; j+=i)
                 prime[j] = false;
 }
 
